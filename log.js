64dcf8ccf17066c3259ee27e0186239a905da22d
zhuofeijian <zhuofeijian@gmail.com>
2022-07-09 16:02:05 +0800

    Update log.js
    change dateFormat

--- log.js
+++ log.js
@@ -1,7 +1,42 @@
 var fs = require('fs');
 var mkdirp = require('mkdirp');
-var dateFormat = require('dateformat');
 var PATH = require('path');
+
+
+const dateFormat = function(date,fmt) {
+    if(typeof date == 'number'){
+        date = new Date(date);
+    }
+    var o = { 
+       "M+" : date.getMonth()+1,                 //月份 
+       "d+" : date.getDate(),                    //日 
+       "h+" : date.getHours(),                   //小时 
+       "m+" : date.getMinutes(),                 //分 
+       "s+" : date.getSeconds(),                 //秒 
+       "q+" : Math.floor((date.getMonth()+3)/3), //季度 
+      // "S"  : date.getMilliseconds()             //毫秒 
+    };
+
+    if(/(y+)/.test(fmt)) {
+        fmt=fmt.replace(RegExp.$1, (date.getFullYear()+"").substr(4 - RegExp.$1.length)); 
+    }
+
+    for(var k in o) {
+        if(new RegExp("("+ k +")").test(fmt)){
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+        }
+    }
+
+    if(/(S+)/.test(fmt)) {
+        var s = date.getMilliseconds();
+        fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (s) : (("000"+ s).substr((""+ s).length)));
+    }
+
+
+    return fmt; 
+}
+
+
 
 var gettodaytime = function(){
 	var date = new Date();
